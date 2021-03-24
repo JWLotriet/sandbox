5827cbbc82f80bef2eb838bc3736d080abe247b5
JWLotriet <johnwilliamlotriet@gmail.com>
2021-03-24 11:34:05 +0200

    finished part 2 of task list

--- DOM-Projects/Task List/app.js
+++ DOM-Projects/Task List/app.js
@@ -15,6 +15,14 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener("submit", addTask);
+  // Removing Task event
+
+  taskList.addEventListener("click", removeTask);
+
+  // Clear Task Events
+  clearBtn.addEventListener("click", clearTasks);
+  // Filter Task Events
+  filter.addEventListener("keyup", filterTasks);
 }
 
 //Add task
@@ -57,4 +65,33 @@ function addTask(e) {
   taskInput.value = "";
 
   e.preventDefault();
+}
+
+//Remove Task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    if (confirm("Are You Sure?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+//Clear Tasks
+function clearTasks(e) {
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+//Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll(".collection-item").forEach(function (task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
 }
